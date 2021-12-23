6f28407509e962c224ecd884122c2370d79633dc
nosd <nosd@zhaw.ch>
2021-12-23 16:05:10 +0100

    convert and check paths in atlas::new

--- tools/atlas/src/lib.rs
+++ tools/atlas/src/lib.rs
@@ -1,37 +1,50 @@
 use std::fs::File;
 use std::io;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 pub mod sym;
 
 use std::process::Command;
 
 #[derive(Debug)]
 pub struct Atlas {
-    elf: PathBuf,
     nm: PathBuf,
+    elf: PathBuf,
+    lib: PathBuf,
 }
 
 impl Atlas {
-    pub fn new<T, U>(elf: T, nm: U) -> Self
+    pub fn new<N,E,L>(nm: N, elf: E, lib: L) -> io::Result<Self>
     where
-        T: Into<PathBuf>,
-        U: Into<PathBuf>,
+        N: AsRef<Path>,
+        E: AsRef<Path>,
+        L: AsRef<Path>,
     {
-        let elf = elf.into();
-        let nm = nm.into();
-        Atlas { elf, nm }
+        let curr = std::env::current_dir().unwrap();
+
+        let nm = curr.join(nm.as_ref()).canonicalize()?;
+        let elf = curr.join(elf.as_ref()).canonicalize()?;
+        let lib = curr.join(lib.as_ref()).canonicalize()?;
+
+        // Check permission by opening and closing files
+        let _ = File::open(&nm)?;
+        let _ = File::open(&elf)?;
+        let _ = File::open(&lib)?;
+
+        Ok(Atlas { nm, elf, lib })
     }
 
     pub fn analyze(&self) -> io::Result<()> {
-        let _f = File::open(&self.elf)?;
-        let out = Command::new(&self.nm).arg(&self.elf).output().unwrap();
+        let out = Command::new(&self.nm).arg(&self.elf).output()?;
+        if !out.status.success() {
+            return Err(io::Error::new(io::ErrorKind::Other, "nm returned an error"));
+        }
 
-        // println!("{:?}",&out);
+        println!("{:?}",&out);
         let out_string = String::from_utf8_lossy(&out.stdout);
         for l in out_string.lines() {
             println!("{}", l);
         }
-        // println!("output: {:?}", out_string);
+        
         Ok(())
     }
 }
@@ -40,62 +53,63 @@ impl Atlas {
 mod tests {
     use super::*;
     use std::io::ErrorKind;
-    use std::path::Path;
-
-    const NM_PATH: &str = "/home/mario/tools/gnuarmemb-9-2019-q4-major/bin/arm-none-eabi-nm";
+    use lazy_static::lazy_static;
+    lazy_static! {
+        static ref NM_PATH: String = std::env::var("NM_PATH").expect("NM_PATH env var not found!");
+    }
 
     #[test]
     fn new_str() {
-        let _at = Atlas::new(file!(), NM_PATH);
+        let at = Atlas::new(&*NM_PATH, file!(), file!());
+        assert!(at.is_ok());
     }
 
     #[test]
     fn new_string() {
-        let _at = Atlas::new(String::from(file!()), NM_PATH);
+        let at = Atlas::new(&*NM_PATH, String::from(file!()), String::from(file!()));
+        assert!(at.is_ok());
     }
 
     #[test]
     fn new_pathbuf() {
-        let _at = Atlas::new(PathBuf::from(file!()), NM_PATH);
+        let at = Atlas::new(&*NM_PATH, PathBuf::from(file!()), PathBuf::from(file!()));
+        assert!(at.is_ok());
     }
 
     #[test]
     fn new_path() {
-        let _at = Atlas::new(Path::new(file!()), NM_PATH);
+        let at = Atlas::new(&*NM_PATH, Path::new(file!()), Path::new(file!()));
+        assert!(at.is_ok());
+    }
+
+    #[test]
+    fn new_mixed() {
+        let at = Atlas::new(&*NM_PATH, Path::new(file!()), file!());
+        assert!(at.is_ok());
+    }
+
+    #[test]
+    fn new_canonicalize() {
+        let at = Atlas::new(&*NM_PATH, "/etc/hostname", "./aux/../src/../Cargo.toml");
+        assert!(at.is_ok());
     }
 
     #[test]
     fn illegal_path() {
-        let err = Atlas::new("./0illegal.elf", NM_PATH).analyze().unwrap_err();
+        let err = Atlas::new(&*NM_PATH, "kljsdflkjsdf", "ljksdflkjsdflsj").unwrap_err();
         assert_eq!(err.kind(), ErrorKind::NotFound);
     }
 
     #[test]
     fn permission_denied() {
-        let err = Atlas::new("/etc/shadow", NM_PATH).analyze().unwrap_err();
+        let err = Atlas::new(&*NM_PATH, file!(), "/etc/shadow").unwrap_err();
         assert_eq!(err.kind(), ErrorKind::PermissionDenied);
     }
 
     #[test]
-    fn analyze_example() {
-        let mut elf = std::env::current_dir().unwrap();
-        elf.push("./aux/rust_minimal_node.elf");
-        let elf = elf.canonicalize().unwrap();
-
-        let at = Atlas::new(elf, NM_PATH);
-        at.analyze().unwrap();
-    }
-
-    #[test]
-    fn temp() {
-        let mut elf = std::env::current_dir().unwrap();
-        println!("{}", elf.display());
-        elf.push("./aux/rust_minimal_node.elf");
-        println!("{}", elf.display());
-        let elf = elf.canonicalize().unwrap();
-        println!("{}", elf.display());
-
-        // let at = Atlas::new(elf, NM_PATH);
-        // at.analyze();
+    fn nm_wrong_file_type() {
+        let at = Atlas::new(&*NM_PATH, "../README.md", "../README.md").unwrap();
+        let err = at.analyze().unwrap_err();
+        assert_eq!(err.kind(), ErrorKind::Other);
     }
 }
