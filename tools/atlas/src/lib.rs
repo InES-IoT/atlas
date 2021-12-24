c60b3ca960b09c3706c7a6f431b503006c99d324
nosd <nosd@zhaw.ch>
2021-12-24 08:22:08 +0100

    first implementation of atlas::analyze method

--- tools/atlas/src/lib.rs
+++ tools/atlas/src/lib.rs
@@ -9,40 +9,77 @@ use std::process::Command;
 pub struct Atlas {
     nm: PathBuf,
     elf: PathBuf,
+    lock: PathBuf,
     lib: PathBuf,
+    syms: Vec<sym::SymbolGuess>,
+    fails: Vec<(String,String)>,
 }
 
 impl Atlas {
-    pub fn new<N,E,L>(nm: N, elf: E, lib: L) -> io::Result<Self>
+    pub fn new<N,E,O,L>(nm: N, elf: E, lock: O, lib: L) -> io::Result<Self>
     where
         N: AsRef<Path>,
         E: AsRef<Path>,
+        O: AsRef<Path>,
         L: AsRef<Path>,
     {
         let curr = std::env::current_dir().unwrap();
 
         let nm = curr.join(nm.as_ref()).canonicalize()?;
         let elf = curr.join(elf.as_ref()).canonicalize()?;
+        let lock = curr.join(lock.as_ref()).canonicalize()?;
         let lib = curr.join(lib.as_ref()).canonicalize()?;
 
         // Check permission by opening and closing files
         let _ = File::open(&nm)?;
         let _ = File::open(&elf)?;
+        let _ = File::open(&lock)?;
         let _ = File::open(&lib)?;
 
-        Ok(Atlas { nm, elf, lib })
+    Ok(Atlas { nm, elf, lock, lib, syms: Vec::new(), fails: Vec::new() })
     }
 
-    pub fn analyze(&self) -> io::Result<()> {
-        let out = Command::new(&self.nm).arg(&self.elf).output()?;
-        if !out.status.success() {
+    pub fn analyze(&mut self) -> io::Result<()> {
+        let mut gsr = sym::Guesser::new();
+        gsr.from_lock(&self.lock).unwrap();
+        gsr.from_lib(&self.nm, &self.lib).unwrap();
+
+        let mangled_out = Command::new(&self.nm)
+            .arg("--print-size")
+            .arg("--size-sort")
+            .arg(&self.elf)
+            .output()?;
+
+        if !mangled_out.status.success() {
             return Err(io::Error::new(io::ErrorKind::Other, "nm returned an error"));
         }
 
-        println!("{:?}",&out);
-        let out_string = String::from_utf8_lossy(&out.stdout);
-        for l in out_string.lines() {
-            println!("{}", l);
+        let mangled_str = std::str::from_utf8(&mangled_out.stdout)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+
+        let demangled_out = Command::new(&self.nm)
+            .arg("--print-size")
+            .arg("--size-sort")
+            .arg("--demangle")
+            .arg(&self.elf)
+            .output()?;
+
+        if !demangled_out.status.success() {
+            return Err(io::Error::new(io::ErrorKind::Other, "nm returned an error"));
+        }
+
+        let demangled_str = std::str::from_utf8(&demangled_out.stdout)
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
+
+        for (mangled, demangled) in mangled_str.lines().zip(demangled_str.lines()) {
+            let guess = match gsr.guess(mangled, demangled) {
+                Ok(g) => g,
+                Err(_) => {
+                    self.fails.push((String::from(mangled), String::from(demangled)));
+                    continue;
+                },
+            };
+            self.syms.push(guess);
         }
         
         Ok(())
@@ -60,56 +97,67 @@ mod tests {
 
     #[test]
     fn new_str() {
-        let at = Atlas::new(&*NM_PATH, file!(), file!());
+        let at = Atlas::new(&*NM_PATH, file!(), file!(), file!());
         assert!(at.is_ok());
     }
 
     #[test]
     fn new_string() {
-        let at = Atlas::new(&*NM_PATH, String::from(file!()), String::from(file!()));
+        let at = Atlas::new(&*NM_PATH, String::from(file!()), String::from(file!()), String::from(file!()));
         assert!(at.is_ok());
     }
 
     #[test]
     fn new_pathbuf() {
-        let at = Atlas::new(&*NM_PATH, PathBuf::from(file!()), PathBuf::from(file!()));
+        let at = Atlas::new(&*NM_PATH, PathBuf::from(file!()), PathBuf::from(file!()), PathBuf::from(file!()));
         assert!(at.is_ok());
     }
 
     #[test]
     fn new_path() {
-        let at = Atlas::new(&*NM_PATH, Path::new(file!()), Path::new(file!()));
+        let at = Atlas::new(&*NM_PATH, Path::new(file!()), Path::new(file!()), Path::new(file!()));
         assert!(at.is_ok());
     }
 
     #[test]
     fn new_mixed() {
-        let at = Atlas::new(&*NM_PATH, Path::new(file!()), file!());
+        let at = Atlas::new(&*NM_PATH, PathBuf::from(file!()), Path::new(file!()), file!());
         assert!(at.is_ok());
     }
 
     #[test]
     fn new_canonicalize() {
-        let at = Atlas::new(&*NM_PATH, "/etc/hostname", "./aux/../src/../Cargo.toml");
+        let at = Atlas::new(&*NM_PATH, "/etc/hostname", "./aux/../src/../Cargo.toml", "src/lib.rs");
         assert!(at.is_ok());
     }
 
     #[test]
     fn illegal_path() {
-        let err = Atlas::new(&*NM_PATH, "kljsdflkjsdf", "ljksdflkjsdflsj").unwrap_err();
+        let err = Atlas::new(&*NM_PATH, "kljsdflkjsdf", "ljksdflkjsdflsj", "lkjsdflkjsdf").unwrap_err();
         assert_eq!(err.kind(), ErrorKind::NotFound);
     }
 
     #[test]
     fn permission_denied() {
-        let err = Atlas::new(&*NM_PATH, file!(), "/etc/shadow").unwrap_err();
+        let err = Atlas::new(&*NM_PATH, file!(), file!(), "/etc/shadow").unwrap_err();
         assert_eq!(err.kind(), ErrorKind::PermissionDenied);
     }
 
     #[test]
     fn nm_wrong_file_type() {
-        let at = Atlas::new(&*NM_PATH, "../README.md", "../README.md").unwrap();
+        let mut at = Atlas::new(&*NM_PATH, "../README.md", "aux/Cargo.lock", "aux/libsecprint.a").unwrap();
         let err = at.analyze().unwrap_err();
         assert_eq!(err.kind(), ErrorKind::Other);
     }
+
+    #[test]
+    fn analyze() {
+        let mut at = Atlas::new(&*NM_PATH, "aux/rust_minimal_node.elf", "aux/Cargo.lock", "aux/libsecprint.a").unwrap();
+        assert!(at.analyze().is_ok());
+        println!("{}", at.syms.len());
+        println!("{}", at.fails.len());
+
+        // println!("{:#?}",at.syms);
+    }
 }
+
